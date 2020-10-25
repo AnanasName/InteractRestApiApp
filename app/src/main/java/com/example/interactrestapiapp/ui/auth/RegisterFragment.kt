7c4b4d969ff3cccf23e49df02001e712abf4636d
AnanasName <56173538+AnanasName@users.noreply.github.com>
2020-10-25 11:40:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/interactrestapiapp/ui/auth/RegisterFragment.kt
@@ -0,0 +1,25 @@
+package com.example.interactrestapiapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.interactrestapiapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegisterFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+}
